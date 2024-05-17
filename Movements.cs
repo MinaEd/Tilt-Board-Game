6d6ba1fabbf5a6680c5a7545b84faaa9cc820375
MinaEd <98766100+MinaEd@users.noreply.github.com>
2024-05-17 11:55:38 +0300

    Add files via upload

---
+++ Movements.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiltGame
+{
+    class Movements
+    {
+        public static Tuple<int, int> target;
+        public static bool finalStateReached = false;
+        public static int finalState = -1;
+        public static char[,] emptyMatrix;
+        public struct state
+        {
+            public List<Tuple<int, int>> indicesOfSliders;
+            public char predecessor;
+        }
+        
+        static public Dictionary<int, Tuple<int, char>> Parentof;
+
+        static public int ConvertStateToInt(state theState, int sizeOfBoard)
+        {
+            int stateInt = 0;
+            
+
+            foreach (var slider in theState.indicesOfSliders)
+            {
+                int index = slider.Item1 * sizeOfBoard + slider.Item2;
+                stateInt += index * index + 1039;
+
+                
+            }
+
+            return stateInt;
+        }
+
+        
+        static bool IsStateVisited(state theState, int parent, int sizeOfBoard)
+        {
+            
+            int Hashedstate = ConvertStateToInt(theState, sizeOfBoard);
+
+            
+            if (Parentof.ContainsKey(Hashedstate))
+            {
+                
+                return true;
+            }
+            else
+            {
+                
+                Parentof.Add(Hashedstate, Tuple.Create(parent, theState.predecessor));
+                return false;
+            }
+        }
+        public static state moveDown(state currentState, int sizeOfBoard, int numOfSliders)
+        {
+            int r, c;
+            state nextState = new state();
+            nextState.indicesOfSliders = new List<Tuple<int, int>>();
+            nextState.predecessor = 'd';
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = currentState.indicesOfSliders[i].Item1;
+                c = currentState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = 'o';
+            }
+            int lastR = sizeOfBoard - 1;
+            for (int j = 0; j < sizeOfBoard; j++)
+            {
+                lastR = sizeOfBoard - 1;
+                for (int i = sizeOfBoard - 1; i >= 0; i--)
+                {
+                    if (emptyMatrix[i, j] == '#')
+                    {
+                        lastR = i - 1;
+                    }
+                    else if (emptyMatrix[i, j] == 'o')
+                    {
+                        emptyMatrix[lastR, j] = 'o';
+                        nextState.indicesOfSliders.Add(new Tuple<int, int>(lastR, j));
+                        if (lastR != i)
+                            emptyMatrix[i, j] = '.';
+                        lastR--;
+                    }
+                }
+            }
+            //nextState.indicesOfSliders.Sort();
+            if (emptyMatrix[target.Item1, target.Item2] == 'o')
+            {
+                finalStateReached = true;
+
+            }
+            if (finalStateReached && finalState == -1)
+            {
+                finalState = ConvertStateToInt(nextState, sizeOfBoard);
+
+            }
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = nextState.indicesOfSliders[i].Item1;
+                c = nextState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = '.';
+            }
+            return nextState;
+        }
+        public static state moveRight(state currentState, int sizeOfBoard, int numOfSliders)
+        {
+            int r, c;
+
+            state nextState = new state();
+            nextState.indicesOfSliders = new List<Tuple<int, int>>();
+            nextState.predecessor = 'r';
+            int lastc = sizeOfBoard - 1;
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = currentState.indicesOfSliders[i].Item1;
+                c = currentState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = 'o';
+            }
+            for (int i = 0; i < sizeOfBoard; i++)
+            {
+                lastc = sizeOfBoard - 1;
+                for (int j = sizeOfBoard - 1; j >= 0; j--)
+                {
+                    if (emptyMatrix[i, j] == '#')
+                    {
+                        lastc = j - 1;
+                    }
+                    else if (emptyMatrix[i, j] == 'o')
+                    {
+                        emptyMatrix[i, lastc] = 'o';
+                        nextState.indicesOfSliders.Add(new Tuple<int, int>(i, lastc));
+                        if (lastc != j)
+                            emptyMatrix[i, j] = '.';
+                        lastc--;
+                    }
+                }
+            }
+            //nextState.indicesOfSliders.Sort();
+            if (emptyMatrix[target.Item1, target.Item2] == 'o')
+            {
+                finalStateReached = true;
+
+            }
+            if (finalStateReached && finalState == -1)
+            {
+                finalState = ConvertStateToInt(nextState, sizeOfBoard);
+
+            }
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = nextState.indicesOfSliders[i].Item1;
+                c = nextState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = '.';
+            }
+            return nextState;
+        }
+
+        public static state moveUp(state currentState, int sizeOfBoard, int numOfSliders)
+        {
+            int r, c;
+            state nextState = new state();
+            nextState.indicesOfSliders = new List<Tuple<int, int>>();
+            nextState.predecessor = 'u';
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = currentState.indicesOfSliders[i].Item1;
+                c = currentState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = 'o';
+            }
+            int lastr = 0;
+            for (int j = 0; j < sizeOfBoard; j++)
+            {
+                lastr = 0;
+                for (int i = 0; i < sizeOfBoard; i++)
+                {
+                    if (emptyMatrix[i, j] == '#')
+                    {
+                        lastr = i + 1;
+                    }
+                    else if (emptyMatrix[i, j] == 'o')
+                    {
+                        emptyMatrix[lastr, j] = 'o';
+                        nextState.indicesOfSliders.Add(new Tuple<int, int>(lastr, j));
+                        if (lastr != i)
+                            emptyMatrix[i, j] = '.';
+                        lastr++;
+                    }
+
+                }
+            }
+            //nextState.indicesOfSliders.Sort();
+            if (emptyMatrix[target.Item1, target.Item2] == 'o')
+            {
+                finalStateReached = true;
+
+            }
+            if (finalStateReached && finalState == -1)
+            {
+                finalState = ConvertStateToInt(nextState, sizeOfBoard);
+
+            }
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = nextState.indicesOfSliders[i].Item1;
+                c = nextState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = '.';
+            }
+            return nextState;
+        }
+        public static state moveLeft(state currentState, int sizeOfBoard, int numOfSliders)
+        {
+            int r, c;
+            state nextState = new state();
+            nextState.indicesOfSliders = new List<Tuple<int, int>>();
+            nextState.predecessor = 'l';
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = currentState.indicesOfSliders[i].Item1;
+                c = currentState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = 'o';
+            }
+            int lastc = 0;
+            for (int i = 0; i < sizeOfBoard; i++)
+            {
+                lastc = 0;
+                for (int j = 0; j < sizeOfBoard; j++)
+                {
+                    if (emptyMatrix[i, j] == '#')
+                    {
+                        lastc = j + 1;
+                    }
+                    else if (emptyMatrix[i, j] == 'o')
+                    {
+                        emptyMatrix[i, lastc] = 'o';
+                        nextState.indicesOfSliders.Add(new Tuple<int, int>(i, lastc));
+                        if (lastc != j)
+                            emptyMatrix[i, j] = '.';
+                        lastc++;
+                    }
+                }
+            }
+            //nextState.indicesOfSliders.Sort();
+            if (emptyMatrix[target.Item1, target.Item2] == 'o')
+            {
+                finalStateReached = true;
+
+            }
+            for (int i = 0; i < numOfSliders; i++)
+            {
+                r = nextState.indicesOfSliders[i].Item1;
+                c = nextState.indicesOfSliders[i].Item2;
+                emptyMatrix[r, c] = '.';
+            }
+            if (finalStateReached && finalState == -1)
+            {
+                finalState = ConvertStateToInt(nextState, sizeOfBoard);
+
+            }
+            return nextState;
+        }
+        static void move(state currentState, int sizeOfBoard, int numOfSliders, ref Queue<state> queueOfStates)
+        {
+
+
+            state returnedState;
+            int parent = ConvertStateToInt(currentState, sizeOfBoard);
+            if (currentState.predecessor == 'i')
+            {
+
+                Parentof.Add(parent, Tuple.Create(-1, 'i'));
+            }
+            if (currentState.predecessor == 'l' || currentState.predecessor == 'r' || currentState.predecessor == 'i')
+            {
+
+                returnedState = moveUp(currentState, sizeOfBoard, numOfSliders);
+
+                ///check here
+                if (!IsStateVisited(returnedState, parent, sizeOfBoard))
+                    queueOfStates.Enqueue(returnedState);
+
+                returnedState = moveDown(currentState, sizeOfBoard, numOfSliders);
+                ///check here
+                if (!IsStateVisited(returnedState, parent, sizeOfBoard))
+                    queueOfStates.Enqueue(returnedState);
+            }
+            if (currentState.predecessor == 'u' || currentState.predecessor == 'd' || currentState.predecessor == 'i')
+            {
+                returnedState = moveLeft(currentState, sizeOfBoard, numOfSliders);
+                ///check here
+                if (!IsStateVisited(returnedState, parent, sizeOfBoard))
+                    queueOfStates.Enqueue(returnedState);
+
+                returnedState = moveRight(currentState, sizeOfBoard, numOfSliders);
+                ///check here
+                if (!IsStateVisited(returnedState, parent, sizeOfBoard))
+                    queueOfStates.Enqueue(returnedState);
+            }
+
+
+        }
+        public static Stack<char> sequence;
+        public static int minNumOfMoves;
+        public static void BFS(state currentState, int sizeOfBoard, int numOfSliders)
+        {
+
+         
+            minNumOfMoves = 0;
+            Queue<state> queueOfStates = new Queue<state>();
+            finalStateReached = false;
+            finalState = -1;
+            Parentof = new Dictionary<int, Tuple<int, char>>();
+            currentState.predecessor = 'i';
+            move(currentState, sizeOfBoard, numOfSliders, ref queueOfStates);
+            while (queueOfStates.Count > 0)
+            {
+                if (finalStateReached)
+                {
+                    Console.WriteLine("Solvable");
+                    sequence = new Stack<char>();
+
+                    Tuple<int, char> t = new Tuple<int, char>(-1, 'i');
+
+                    for (Tuple<int, char> i = Parentof[finalState]; !i.Equals(t); i = Parentof[i.Item1])
+                    {
+                        minNumOfMoves++;
+                        sequence.Push(i.Item2);
+                    }
+                    Console.WriteLine("Min number of moves: " + minNumOfMoves);
+                    Console.Write("Sequence of moves: ");
+                    return;
+                }
+                currentState = queueOfStates.Dequeue();
+                
+                move(currentState, sizeOfBoard, numOfSliders, ref queueOfStates);
+
+            }
+            Console.WriteLine("unsolvable");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static TiltGame.Movements;
+namespace TiltGame
+{
+    class Program
+    {
+        static System.Diagnostics.Stopwatch myStopWatch = new System.Diagnostics.Stopwatch();
+        static void Main(string[] args)
+        {
+            string basePath = @"C:\3rdYear2ndSemester\Algo\Project\[4] Tilt Game\Test Cases";
+            while (true)
+            {
+                Console.WriteLine("Enter 1 for Sample Tests or 2 for Complete Tests:");
+                string userInput = Console.ReadLine();
+
+                string folderPath;
+                if (userInput == "1")
+                {
+                    folderPath = Path.Combine(basePath, "Sample Tests");
+                    ReadFilesFromFolder(folderPath, true);
+                }
+                else if (userInput == "2")
+                {
+                    folderPath = Path.Combine(basePath, "Complete Tests");
+
+                    Console.WriteLine("Choose subfolder:");
+                    Console.WriteLine("1. Small");
+                    Console.WriteLine("2. Medium");
+                    Console.WriteLine("3. Large");
+                    string subfolderChoice = Console.ReadLine();
+
+                    string subfolderName;
+                    if (subfolderChoice == "1")
+                    {
+                        subfolderName = "1 small";
+                    }
+                    else if (subfolderChoice == "2")
+                    {
+                        subfolderName = "2 medium";
+                    }
+                    else if (subfolderChoice == "3")
+                    {
+                        subfolderName = "3 large";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid subfolder choice. Exiting program.");
+                        return;
+                    }
+
+                    folderPath = Path.Combine(basePath, "Complete Tests", subfolderName);
+
+                    
+                    ReadFilesFromFolder(folderPath, false);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Exiting program.");
+                    break;
+                }
+            }
+        }
+
+        static void ReadFilesFromFolder(string folderPath, Boolean isSample)
+        {
+            string[] files = Directory.GetFiles(folderPath, "Case*.txt", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                
+                if (file.EndsWith("-output.txt"))
+                {
+                    continue;
+                }
+                string[] lines = File.ReadAllLines(file);
+
+            
+
+                if (!int.TryParse(lines[0], out int size))
+                {
+
+                    continue; 
+                }
+
+                
+                char[,] matrix = new char[size, size];
+
+                
+                int countOfSliders = 0;
+                int[,] positionsOfSlider = new int[size * size, 2];
+                int c = 0, r = 0;
+                state s = new state();
+
+                s.indicesOfSliders = new List<Tuple<int, int>>();
+
+                for (int i = 0; i < size; i++)
+                {
+                    string[] row = lines[i + 1].Split(',');
+
+                    for (int j = 0; j < size; j++)
+                    {
+                        matrix[i, j] = row[j].Trim()[0]; 
+                        if (matrix[i, j] == 'o')
+                        {
+                            matrix[i, j] = '.';
+                            countOfSliders++;
+                            positionsOfSlider[r, 0] = i;
+                            positionsOfSlider[r++, 1] = j;
+                            s.indicesOfSliders.Add(Tuple.Create(i, j));
+                        }
+
+                    }
+                }
+
+                
+                string[] targetCoords = lines[lines.Length - 1].Split(',');
+                int targetX, targetY;
+                targetX = int.Parse(targetCoords[0]);
+                targetY = int.Parse(targetCoords[1]);
+                target = new Tuple<int, int>(targetY, targetX);
+                finalStateReached = false;
+                finalState = -1;
+                emptyMatrix = matrix;
+
+                myStopWatch.Restart();
+                BFS(s, size, countOfSliders);
+                myStopWatch.Stop();
+                char[] seqArr = new char[minNumOfMoves];
+                int num = 0;
+                while (sequence != null && sequence.Count > 0)
+                {
+                    char dir = sequence.Pop();
+                    seqArr[num] = dir;
+                    num++;
+                    if (dir == 'l')
+                    {
+                        Console.Write("left, ");
+                    }
+                    if (dir == 'r')
+                    {
+                        Console.Write("right, ");
+                    }
+                    if (dir == 'u')
+                    {
+                        Console.Write("up, ");
+                    }
+                    if (dir == 'd')
+                    {
+                        Console.Write("down, ");
+                    }
+
+                }
+                if (isSample && seqArr.Length > 0)
+                {
+                    s = new state();
+                    s.indicesOfSliders = new List<Tuple<int, int>>();
+                    Console.WriteLine("");
+                    Console.WriteLine("intial");
+                    for (int i = 0; i < countOfSliders; i++)
+                    {
+                        emptyMatrix[positionsOfSlider[i, 0], positionsOfSlider[i, 1]] = 'o';
+                        s.indicesOfSliders.Add(Tuple.Create(positionsOfSlider[i, 0], positionsOfSlider[i, 1]));
+
+                    }
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            Console.Write(emptyMatrix[i, j] + ", ");
+                        }
+                        Console.WriteLine("");
+                    }
+                    for (int i = 0; i < countOfSliders; i++)
+                    {
+                        emptyMatrix[positionsOfSlider[i, 0], positionsOfSlider[i, 1]] = '.';
+                    }
+                    for (int i = 0; i < seqArr.Length; i++)
+                    {
+                        Console.WriteLine("---------------------------------");
+                        if (seqArr[i] == 'l')
+                        {
+                            Console.WriteLine("left");
+                            s = moveLeft(s, size, countOfSliders);
+                        }
+                        if (seqArr[i] == 'r')
+                        {
+                            Console.WriteLine("right");
+                            s = moveRight(s, size, countOfSliders);
+                        }
+                        if (seqArr[i] == 'u')
+                        {
+                            Console.WriteLine("up");
+                            s = moveUp(s, size, countOfSliders);
+                        }
+                        if (seqArr[i] == 'd')
+                        {
+                            Console.WriteLine("down");
+                            s = moveDown(s, size, countOfSliders);
+                        }
+                        for (int l = 0; l < countOfSliders; l++)
+                        {
+                            emptyMatrix[s.indicesOfSliders[l].Item1, s.indicesOfSliders[l].Item2] = 'o';
+                        }
+                        for (int k = 0; k < size; k++)
+                        {
+                            for (int j = 0; j < size; j++)
+                            {
+                                Console.Write(emptyMatrix[k, j] + ", ");
+                            }
+                            Console.WriteLine("");
+                        }
+                        for (int l = 0; l < countOfSliders; l++)
+                        {
+                            emptyMatrix[s.indicesOfSliders[l].Item1, s.indicesOfSliders[l].Item2] = '.';
+                        }
+                    }
+                }
+                Console.WriteLine("");
+                Console.WriteLine("the time in seconds is " + myStopWatch.Elapsed.Seconds.ToString());
+                Console.WriteLine("the time in mins is " + myStopWatch.Elapsed.Minutes.ToString());
+                
+                Console.WriteLine("-----------------------------------------------");
+
+                
+
+                Console.WriteLine();
+            }
+
+        }
+
+
+    }
+}
